295ff5891eaa7ae10b397bceb416b067b3a3c570
ChiuLui <906277645@qq.com>
2021-08-04 18:06:40 +0800

    Using state in Jetpack Compose : 8. Extracting stateless composables

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -21,8 +21,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
 
+/**
+ * 主页
+ * 逻辑写在 ViewModel
+ * 界面写在 Compose，界面逻辑通过状态提升，提升到 Activity ，传入 ViewModel 逻辑。
+ * 桥梁写在 Activity
+ */
 class TodoActivity : AppCompatActivity() {
 
     val todoViewModel by viewModels<TodoViewModel>()
@@ -32,9 +41,22 @@ class TodoActivity : AppCompatActivity() {
         setContent {
             StateCodelabTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
+
+    @Composable
+    private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+        // .observeAsState observes a LiveData<T> and converts it into a State<T> object so Compose can react to value changes（观察 LiveData<T> 并将其转换为 State<T> 对象，以便 Compose 可以对值更改做出反应）
+        val items: List<TodoItem> by todoViewModel.todoItems.observeAsState(initial = listOf())// listOf() 是给一个默认值，当返回为空，会给一个空数组
+
+        TodoScreen(
+            items = items,
+            onAddItem = { todoViewModel.addItem(it) },
+            onRemoveItem = { todoViewModel.removeItem(it) }
+        )
+    }
+
 }
