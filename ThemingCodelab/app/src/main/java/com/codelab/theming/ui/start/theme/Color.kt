da0a82ec70caae4f00f48cfa111c23837e225435
ChiuLui <906277645@qq.com>
2021-08-06 17:51:54 +0800

    8. Jetpack Compose theming

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * 主题颜色$
+ * @author    神经大条蕾弟
+ * @date      2021/08/06 11:48
+ */
+
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,18 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * 形状$
+ * @author    神经大条蕾弟
+ * @date      2021/08/06 12:24
+ */
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
